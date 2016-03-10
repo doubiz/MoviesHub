a7b047972e5a0548efa7ac7292b7f9695b311500
Omar Mowafi <omar.mowafi@gmail.com>
2016-03-10 19:43:27 +0200

    added seeds and missing validation in user_movie

--- app/models/user_movie.rb
+++ app/models/user_movie.rb
@@ -4,7 +4,8 @@ class UserMovie < ActiveRecord::Base
 
   ##Validations
   validates_presence_of :movie, :user
-  
+  validates_uniqueness_of :user_id, scope: :movie_id, message: "Already added this movie before"
+
   ##Relations
   belongs_to :movie 
   belongs_to :user
